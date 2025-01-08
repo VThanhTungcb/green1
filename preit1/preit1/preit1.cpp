000ca2d0273cf9683c865b1d72a51e3b712e6a09
vuongthanhtung231104@gmail.com <pc@DESKTOP-KJBOUAL>
2025-01-08 20:29:27 +0700

    Làm bai 1

--- preit1/preit1/preit1.cpp
+++ preit1/preit1/preit1.cpp
@@ -1,11 +1,10 @@
-// preit1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World,I am Thanh Tung !\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
